dba0134e8cb032f977dea32ebc8830dcf400715a
AlexanderDolgan <5704737@gmail.com>
2018-07-03 02:34:35 +0300

    nav mob

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
+var navMob = document.getElementById('nav-mob'),
+		navWidth = '-46.875rem';
 
+function openNav() {
+	event.stopPropagation();
+	navMob.style.right = '0';
+	navMob.classList.add('active-nav');
+	document.body.style.overflow = 'hidden';
+}
+
+
+function closeNav() {
+	event.stopPropagation();
+	navMob.style.right = navWidth;
+	navMob.classList.remove('active-nav');
+	document.body.style.overflow = 'scroll';
+}
+
+document.addEventListener('click', function() {
+	event.stopPropagation();
+	navMob.style.right = navWidth;
+	navMob.classList.remove('active-nav');
+	document.body.style.overflow = 'scroll';
 });
